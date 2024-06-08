475b2170ad938562d9b1e3598cd85e3bb685fead
Christopher Wilson <cmw.wilson88@gmail.com>
2024-06-08 01:12:06 -0500

    downcases string comparison values

--- src/lib/listings.ts
+++ src/lib/listings.ts
@@ -42,12 +42,14 @@ interface Listing {
   laundry: boolean;
 }
 
+const downcase = (str: string) => str?.toLowerCase();
+
 export const filterListings = (listings: Listing[], search: Search) => {
   return listings.filter((listing) => {
-    if (search.city && listing.city !== search.city) {
+    if (search.city && downcase(listing.city) !== downcase(search.city)) {
       return false;
     }
-    if (search.state && listing.state !== search.state) {
+    if (search.state && downcase(listing.state) !== downcase(search.state)) {
       return false;
     }
     if (search.zip && listing.zip !== search.zip) {
@@ -65,7 +67,7 @@ export const filterListings = (listings: Listing[], search: Search) => {
     if (search.sqft && (listing.sqft < (search.sqft.min ?? 0) || listing.sqft > (search.sqft.max ?? Infinity))) {
       return false;
     }
-    if (search.propertyType && listing.property_type !== search.propertyType) {
+    if (search.propertyType && downcase(listing.property_type) !== downcase(search.propertyType)) {
       return false;
     }
     if (search.pets) {
